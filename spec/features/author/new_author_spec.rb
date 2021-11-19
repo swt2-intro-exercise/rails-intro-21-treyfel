348aa9fbad06e563a83822a59f9f7cb447d2fea0
treyfel <58570309+treyfel@users.noreply.github.com>
2021-11-19 14:22:16 +0100

    Failing test for #3.

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,11 @@ require 'rails_helper'
       # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
     visit new_author_path
    end
+   it "should have an input field for new authors, asking for first name, last name and hoepage" do
+     visit new_author_path
+     # these are the standard names given to inputs by the Rails form builder
+     expect(page).to have_field('author[first_name]')
+     expect(page).to have_field('author[last_name]')
+     expect(page).to have_field('author[homepage]')
+   end
  end
