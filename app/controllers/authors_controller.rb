606d3873b885af9ac0576226d1aac503ec7a5e78
treyfel <58570309+treyfel@users.noreply.github.com>
2021-11-24 23:56:06 +0100

    fix test for #11.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -38,6 +38,13 @@ class AuthorsController < ApplicationController
     end
   end
 
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+
+    redirect_to authors_path
+  end
+
   private
 
   def author_params
