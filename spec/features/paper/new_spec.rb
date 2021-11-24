3626f079283a943b559cc71999b56d0ac0a5bb70
treyfel <58570309+treyfel@users.noreply.github.com>
2021-11-25 00:22:38 +0100

    fix test for #14.

--- spec/features/paper/new_spec.rb
+++ spec/features/paper/new_spec.rb
@@ -3,4 +3,8 @@ describe 'New paper page', type: :feature do
     paper = Paper.new(title: '')
     expect(paper).to_not be_valid
   end
+  it 'should fail when the year is not numeric' do
+    paper = Paper.new(title: 'paper', year: 'ago', venue: 'this')
+    expect(paper).to_not be_valid
+  end
 end
