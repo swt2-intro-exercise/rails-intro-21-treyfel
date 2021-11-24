a3d6589298b329993be2d91832286f91fcd50631
treyfel <58570309+treyfel@users.noreply.github.com>
2021-11-25 00:10:57 +0100

    fix test for #12.

--- spec/features/paper/index_spec.rb
+++ spec/features/paper/index_spec.rb
@@ -4,8 +4,8 @@ describe 'Paper index page', type: :feature do
   it 'should show all the papers' do
     paper = create :paper
     visit papers_path
-    expect(page).to have_text 'title '
-    expect(page).to have_text 'venue'
-    expect(page).to have_text 'year'
+    expect(page).to have_text 'Title'
+    expect(page).to have_text 'Venue'
+    expect(page).to have_text 'Year'
   end
 end
